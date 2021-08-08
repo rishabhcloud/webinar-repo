b930da9d0a57925dac22ea0217f74e31abfafe53
Rishabh Sharma <er.rishabh.sharma@gmail.com>
2021-08-08 20:44:12 +1000

    azure guild demo

---
+++ index.js
@@ -0,0 +1,10 @@
+var express = require('express');
+ 
+var app = express();
+var calc = 2;
+app.get('/', function (req, res) {
+ res.send('Welcome To The Azure Guild Webinar, Great People ! And Thanks for taking part in this video.' + "</br>" + 'The result of 1+1 is: ' + calc);
+});
+app.listen(process.env.PORT || 3000);
+ 
+module.exports = app;
